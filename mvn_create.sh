53d5e2ce82ed37404651c4b747498510ada8ea0c
Wuriyanto <wuriyanto48@yahoo.co.id>
2020-01-21 21:43:06 +0700

    add mvn_create

---
+++ mvn_create.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+check_err()
+{
+    if [ "$1" -ne "0" ]; then
+        echo "Error # ${1} : ${2}"
+        exit ${1}
+    fi
+}
+
+gen()
+{
+    PACKAGE_NAME=$1
+    APP_NAME=$2
+
+    if [[ -z "$PACKAGE_NAME" || -z "$APP_NAME" ]]; then
+        echo "$0 require package name and app name arguments"
+        return 1
+    fi
+
+    mvn archetype:generate -DgroupId=$PACKAGE_NAME -DartifactId=$APP_NAME \
+    -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+
+    return 0
+}
+
+gen "$@"
+check_err $? "create maven app returned an error....!"
+
+
+# TODO
+# curl -O https://raw.githubusercontent.com/wuriyanto48/build-tools/master/mvn_create.sh | sh
